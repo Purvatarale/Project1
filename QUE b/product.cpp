375cea6d5139421c2503a15d0d9238f0b6924e22
Purvatarale <78158135+Purvatarale@users.noreply.github.com>
2021-01-29 20:17:42 +0530

    C++ Code

---
+++ QUE b/product.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float s,p,t;
+	cout<<"Enter first number";
+	cin>>s;
+	cout<<"Enter second number";
+	cin>>p;
+	t=s*p;
+	cout<<"The Product is"<<t;
+}
+
+
